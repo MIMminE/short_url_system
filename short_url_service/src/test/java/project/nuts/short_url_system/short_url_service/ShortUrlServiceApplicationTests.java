0b87ea6bd0352d6303378e647bcb9e9f5e6b9536
MIMminE <weluv.ww@gmail.com>
2025-03-19 14:42:15 +0900

    first commit

---
+++ short_url_service/src/test/java/project/nuts/short_url_system/short_url_service/ShortUrlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.nuts.short_url_system.short_url_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
